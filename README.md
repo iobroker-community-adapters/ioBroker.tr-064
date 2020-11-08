783edaca3263acc93888936d13c4728caad086a0
Ingo Fischer <github@fischer-ka.de>
2020-11-08 23:09:47 +0100

    * (Apollon77) fix #168

--- README.md
+++ README.md
@@ -142,6 +142,7 @@ To enable the call monitor dial ```#96*5*```  and the TCP/IP Port 1012 will be o
 * (Apollon77) Try to solve error 500 problem with offline devices
 * (SliX185) Add IPv6 states
 * (foxriver76) optimizations
+* (Apollon77) Fix initialization if ip/host
 
 ### 4.1.0 (2020-09-17)
 * (Apollon77) Fix crash case (Sentry IOBROKER-TR-064-14)
--- main.js
+++ main.js
@@ -1193,7 +1193,7 @@ function main(adapter) {
     deleteUnusedDevices();
     callList.init(adapter, systemData);
 
-    tr064Client = new TR064(adapter.config.user, adapter.config.password, adapter.config.iporhost || adapter.config.ip);
+    tr064Client = new TR064(adapter.config.user, adapter.config.password, adapter.config.ip || adapter.config.iporhost);
     tr064Client.init(err => {
         initError = err;
         if (err) {
